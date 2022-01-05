b92e5617e7c8e15b2baa5df6ca8925d9c38f8ca3
mrazadtamal <mrazadt@gmail.com>
2022-01-05 23:17:08 +0600

    fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import TopBar from '../Shared/TopBar/TopBar';
+import React from "react";
+import TopBar from "../Shared/TopBar/TopBar";
+import "./Contact.css";
 
 const Contact = () => {
-    return (
-        <div>
-            <TopBar />
-            <h1>This is contact Page</h1>
+  return (
+    <div>
+      <TopBar />
+
+      <div className="contact">
+        <div className="c">
+          <div className="c-wrapper">
+            <div className="c-right">
+              <p className="c-desc">Contact Us</p>
+              <form className="c-form">
+                <input
+                  className="c-input"
+                  type="text"
+                  placeholder="Name"
+                  name="user_name"
+                />
+                <input
+                  className="c-input"
+                  type="text"
+                  placeholder="Subject"
+                  name="user_subject"
+                />
+                <input
+                  className="c-input"
+                  type="text"
+                  placeholder="Email"
+                  name="user_email"
+                />
+                <textarea rows="5" placeholder="Message" name="message" />
+                <button className="c-button">Submit</button>
+              </form>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
