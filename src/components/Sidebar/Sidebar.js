e2d2dd4457607408bfa0819d4e4dff0421c55324
CoderMahabub <codermahabub@gmail.com>
2022-01-23 23:40:49 +0600

    Sidebar Fixed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,48 +4,49 @@ import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = () => {
-    const [cats, setCats] = useState([]);
+	const [ cats, setCats ] = useState([]);
 
-    useEffect(() => {
-        const getCats = async () => {
-            const res = await axios.get("/categories");
-            setCats(res.data)
-        }
-        getCats();
-    }, [])
-    return (
-        <div className="sidebar">
-            <div className="sidebarItem">
-                <span className="sidebarTitle">ABOUT ME</span>
-                <img
-                    src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
-                    alt=""
-                />
-                <p>
-                    CrazyBlog is a blog website for daily users who love to read different types of blog posts in a same platform with best quality blogs from a huge number of authors and writers.
-                </p>
-            </div>
-            <div className="sidebarItem">
-                <span className="sidebarTitle">CATEGORIES</span>
-                <ul className="sidebarList">
-                    {cats.map((c) => (
-                        <Link to={`/?cat=${c.name}`} className="link">
-                            <li className="sidebarListItem">{c.name}</li>
-                        </Link>
-                    ))}
-                </ul>
-            </div>
-            <div className="sidebarItem">
-                <span className="sidebarTitle">FOLLOW US</span>
-                <div className="sidebarSocial">
-                    <i className="sidebarIcon fab fa-facebook-square"></i>
-                    <i className="sidebarIcon fab fa-instagram-square"></i>
-                    <i className="sidebarIcon fab fa-pinterest-square"></i>
-                    <i className="sidebarIcon fab fa-twitter-square"></i>
-                </div>
-            </div>
-        </div>
-    );
+	useEffect(() => {
+		const getCats = async () => {
+			const res = await axios.get('/categories');
+			setCats(res.data);
+		};
+		getCats();
+	}, []);
+	return (
+		<div className="sidebar">
+			<div className="sidebarItem">
+				<span className="sidebarTitle">ABOUT US</span>
+				<img
+					src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+					alt=""
+				/>
+				<p>
+					CrazyBlog is a blog website for daily users who love to read different types of blog posts in a same
+					platform with best quality blogs from a huge number of authors and writers.
+				</p>
+			</div>
+			<div className="sidebarItem">
+				<span className="sidebarTitle">CATEGORIES</span>
+				<ul className="sidebarList">
+					{cats.map((c) => (
+						<Link to={`/?cat=${c.name}`} className="link">
+							<li className="sidebarListItem">{c.name}</li>
+						</Link>
+					))}
+				</ul>
+			</div>
+			<div className="sidebarItem">
+				<span className="sidebarTitle">FOLLOW US</span>
+				<div className="sidebarSocial">
+					<i className="sidebarIcon fab fa-facebook-square" />
+					<i className="sidebarIcon fab fa-instagram-square" />
+					<i className="sidebarIcon fab fa-pinterest-square" />
+					<i className="sidebarIcon fab fa-twitter-square" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
